0413b6d16e5f16f5e26edfccde5aac9e09573ca4
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-12 12:18:43 -0700

    Add files via upload

---
+++ Model/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stoack_List.Model
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StockPurchased { get; set; }
+        public DateTime? StockSold { get; set; }
+    }
+}
